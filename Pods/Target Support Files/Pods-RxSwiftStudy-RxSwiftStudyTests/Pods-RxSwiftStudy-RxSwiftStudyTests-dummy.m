acc2f6771654a883f92e5d33802ee017afe87e51
cjy <changjun.yang@nhnent.com>
2019-12-05 14:12:56 +0900

    remove all

--- Pods/Target Support Files/Pods-RxSwiftStudy-RxSwiftStudyTests/Pods-RxSwiftStudy-RxSwiftStudyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxSwiftStudy_RxSwiftStudyTests : NSObject
-@end
-@implementation PodsDummy_Pods_RxSwiftStudy_RxSwiftStudyTests
-@end
--- Pods/Target Support Files/Pods-RxSwiftStudy-RxSwiftStudyUITests/Pods-RxSwiftStudy-RxSwiftStudyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxSwiftStudy_RxSwiftStudyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RxSwiftStudy_RxSwiftStudyUITests
-@end
